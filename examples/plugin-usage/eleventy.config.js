fd7f3268672bd3ba3ee0d584582d46981276814a
Anton Staroverov <73071319+tonystar@users.noreply.github.com>
2026-07-25 12:27:25 -0600

    offload starters to buildawesome-micro-starters

--- examples/plugin-usage/eleventy.config.js
+++
@@ -1,5 +0,0 @@
-import eleventyBladesPlugin from "@anyblades/eleventy-blades";
-
-export default function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyBladesPlugin);
-}
--- examples/plugin-usage/index.md
+++
@@ -1,1 +0,0 @@
-Hello, world! © {{ site.year }} 🥷
